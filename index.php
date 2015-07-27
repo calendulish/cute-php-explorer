7b756464fa012356b64286c836e81924f9766850
Lara Maia <dev@lara.click>
2015-07-26 21:33:29 -0300

    First commit of login/logout system :sleepy:

--- index.php
+++ index.php
@@ -17,11 +17,54 @@
  *
  */
 
+session_start();
+
 include_once("config.php");
 include_once("engine.php");
 
 $CuteExplorer = new CuteExplorer();
 $CuteExplorer->read_dir();
+
+// If the user try to login, check if the password is correct.
+if(isset($_POST['login'])) {
+    $user_input = $_POST['passwd'];
+    // If the user exists, check if the password is correct
+    if(array_key_exists($_POST['user'], $CuteExplorer->get_config('users'))) {
+        $password = $CuteExplorer->get_config("users")[$_POST['user']];
+        if(crypt($user_input, $password) == $password) {
+            $_SESSION['users'] = $_POST['user'];
+        }
+    }
+}
+// If the user try to logout, so logout and reload the page.
+if(isset($_POST['logout'])&&isset($_SESSION['users'])) {
+    unset($_SESSION['users']);
+    header('Location: index.php');
+    exit(0);
+}
+
+// If the user try to access a hidden directory, he needs a special
+// permission for that. Check if he is currently logged in.
+if($CuteExplorer->get_value('dir')) {
+    $directories = explode('/', $CuteExplorer->get_value('dir'));
+    // Remove empty element case the first directory are a '/'
+    if($directories[0] == "") array_shift($directories);
+    // Check if the first directory exists in hidden_dirs, if not
+    // check all subdirectories until a match is found.
+    $directory = array($directories[0]);
+    for($e=0;$e<count($directories);$e++) {
+        if($e!=0) array_push($directory, "/", $directories[$e]);
+        if(in_array(implode($directory), $CuteExplorer->get_config('hidden_dirs'))) {
+            // If a match is found, check if user is logged.
+            //If not, back and shows a info mesage.
+            if(!isset($_SESSION['users'])) {
+                header('Location: index.php?error_code=403');
+                exit;
+            }
+            break;
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +79,41 @@ $CuteExplorer->read_dir();
 
 <body>
     <h1 class="title"><?=$CuteExplorer->get_config('title')?></h1>
-<?php if($CuteExplorer->get_value('dir')) {
+<?php
+// If the user is not logged in, show the login form.
+if(!isset($_SESSION['users'])) {
+// If the user tried access a protected file or folder,
+// show the info message.
+if($CuteExplorer->get_value('error_code') == 403) {
+    printf("%4s%s\n", "", "<p>You don't have permission to access the requested page or file.</p>");
+    printf("%4s%s\n", "", "<p>Please, if you need to access, login in the form bellow.</p>");
+}
+?>
+    <form method="post" action="">
+        <p class="login">
+            User: <input type="text" pattern=".{3,}" required title="You need at least 3 characters" name="user" size="20">
+            Password: <input type="password" pattern=".{5,}" required title="You need at least 5 characters" name="passwd" size="20">
+            <input type="submit" name="login" value="Login">
+        </p>
+    </form>
+<?php
+    // If the user or password is incorrect, show a info message.
+    if(isset($_POST['login'])) {
+        printf("%8s%s", "", "Incorrect login. Try again.");
+    }
+// If the user is already logged in, show the user info and logout button.
+} else {
+?>
+    <form method="post" action="">
+        <p>
+            You are logged in as <?=$_SESSION['users']?>.
+            <input type="submit" name="logout" value="Logout">
+        </p>
+    </form>
+<?php
+}
+
+if($CuteExplorer->get_value('dir')) {
     printf("%4s%s\n", "", "<p class='current_directory'>~".$CuteExplorer->get_value('dir')."</p>");
 } else {
     printf("%4s%s\n", "", "<p class='current_directory'>~/</p>");
