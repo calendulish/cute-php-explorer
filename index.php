673482f7c3eb2ddd9ff5ea68668220803a24bbfd
Lara Maia <dev@lara.click>
2015-07-31 16:26:05 -0300

    Update comments

--- index.php
+++ index.php
@@ -1,30 +1,13 @@
 <?php
-/*
- * The Cute PHP explorer is free software: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * The Cute PHP explorer is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Lara Maia <dev@lara.click> © 2015
- *
- */
+// Think of it like a sample file
 
-// Use the file cute-php-explorer/config.php for setting configure
-// options for cute-php-explorer.
+// Use the file config.php for setting configure options
+// for cute-php-explorer.
 
 // You can put anything in this file, and it don't needs to be updated.
-// Only update files from cute-php-explorer folder. Remember to backing
-// up your config.php before update.
+// Only update files from cute-php-explorer folder.
 
-// don't write anything before that include
+// don't write anything before these includes
 include_once('config.php');
 include_once('cute-php-explorer/init.php');
 // Here you can put your php functions
